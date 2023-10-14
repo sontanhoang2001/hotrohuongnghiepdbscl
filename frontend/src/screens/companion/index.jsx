26a8318bdadc6593a07240c8df94aad524b6c2b4
sontanhoang2001 <sontanhoang2001@gmail.com>
2023-10-14 17:43:31 +0700

    new branch deployServer

--- frontend/src/screens/companion/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Companion() {
-  return <div>đồng hành</div>;
-}
-
-export default Companion;
--- frontend/src/screens/dashboard/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Dashboard() {
-  return <div>abc</div>;
-}
-
-export default Dashboard;
