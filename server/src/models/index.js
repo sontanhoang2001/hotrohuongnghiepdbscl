62ff1ec432bb4ae9317ee5ed68453b08b49e6909
sontanhoang2001 <sontanhoang2001@gmail.com>
2023-10-14 17:11:10 +0700

    test fix deploy vercel v1

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -12,9 +12,15 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: 'mysql',
+    dialectModule: require('mysql2')
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    dialect: 'mysql',
+    dialectModule: require('mysql2')
+  });
 }
 
 fs
