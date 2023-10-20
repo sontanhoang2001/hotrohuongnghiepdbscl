e06195a71eac615b42c7ae204405ac7e791e0d9c
sontanhoang2001 <sontanhoang2001@gmail.com>
2023-10-20 18:01:06 +0700

    add file config axios

---
+++ src/api/auth.js
@@ -0,0 +1,36 @@
+import axiosClient from "./axiosClient";
+
+const BASE_URL = `${process.env.REACT_APP_API_URL}`;
+const authApi = {
+  login: async (payload) => {
+    const url = `${BASE_URL}/auth/login`;
+    return await axiosClient.post(url, payload);
+  },
+  register: async (payload) => {
+    const url = `${BASE_URL}/api/register`;
+    const rs= await axiosClient.post(url, payload);
+    console.log("regis:",rs);
+    return rs;
+  },
+  getDetails: async (payload) => {
+    const url = `${BASE_URL}/api/findaccountbyusername`;
+   const rs=await axiosClient.post(url, payload);
+   return rs;
+  },
+  updateProfile: async (payload) => {
+    const url = `${BASE_URL}/api/updateinforaccount`;
+    const rs =  await axiosClient.post(url, payload);
+    console.log("rs: ", rs)
+    return rs;
+  },
+  changePassword: async (payload) => {
+    const url = `${BASE_URL}/api/changepassword`;
+    return await axiosClient.post(url, payload);
+  },
+  changeStatus: async (payload) => {
+    const url = `${BASE_URL}/admin/updatestatusaccount`;
+    return await axiosClient.post(url, payload);
+  },
+};
+
+export default authApi;
