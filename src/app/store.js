dc3dfd0a3630d6114525246fce2ca8d62df53123
sontanhoang2001 <sontanhoang2001@gmail.com>
2023-10-23 09:12:27 +0700

    config store v1

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,36 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../redux/counter/counterSlice';
 import authReducer from '../redux/authSlice';
 
+// Auth middleware
+const authMiddleware = store => next => action => {
+
+  const {auth} = store.getState();
+
+  console.log(action);
+
+  // Chưa đăng nhập thì chặn lại
+  // if(!auth.isAuthenticated && action.type !== 'auth/login') {
+  //   console.log('User is not authenticated');
+  //   // throw new Error('User is not authenticated'); 
+  // }
+
+  // return next(action);
+}
+
+// Error middleware
+const errorMiddleware = store => next => action => {
+  try {
+    return next(action);
+  } catch (err) {
+    console.error('Caught error', err);
+    // dispatch error action here    
+  }
+}
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer,
   },
+  middleware: [authMiddleware],
 });
