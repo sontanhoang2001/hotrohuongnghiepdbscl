71c67da3367ef37c41985dd5754902b12fc3385a
A18156 <laughingnguyen15@gmail.co>
2023-11-07 09:47:44 +0700

    rename axiosClient -- update

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,31 +1,31 @@
-import axiosService from './axiosClient';
+import axiosClient from './axiosClient';
 
 const pathname = 'auth';
 
 const authApi = {
   signin: (payload) => {
     const url = `/${pathname}/login`;
-    return axiosService.post(url, payload);
+    return axiosClient.post(url, payload);
   },
   signup: (payload) => {
     const url = `/${pathname}/registerUser`;
-    return axiosService.post(url, payload);
+    return axiosClient.post(url, payload);
   },
   requestOTP: (payload) => {
     const url = `/${pathname}/requestOTP`;
-    return axiosService.post(url, payload);
+    return axiosClient.post(url, payload);
   },
   authOTP: (payload) => {
     const url = `/${pathname}/authOTP`;
-    return axiosService.post(url, payload);
+    return axiosClient.post(url, payload);
   },
   authChangeEmail: (payload) => {
     const url = `/${pathname}/authChangeEmail`;
-    return axiosService.post(url, payload);
+    return axiosClient.post(url, payload);
   },
   changePassword: (payload) => {
     const url = `/${pathname}/changePassword`;
-    return axiosService.post(url, payload);
+    return axiosClient.post(url, payload);
   },
 };
 
